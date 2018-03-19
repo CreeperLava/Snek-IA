edebbd62593b81314ebd067a9411e9c159982818
antoinezak <antoinezak195@gmail.com>
2018-03-19 15:31:25 +0100

    19/03

--- snek.py
+++ snek.py
@@ -55,22 +55,71 @@ class game:
 		# If snake runs over itself
 		if snake[0] in snake[1:]: break
 
-		if snake[0] == food:											# When snake eats the food
-			food = []
-			score += 1
-			while food == []:
-				food = [randint(1, size_y - 2), randint(1, size_x - 2)]				 # Calculating next food's coordinates
+		if snake[0] == food: 
+			self.eat_food()
+		#else: last = snake.pop()										
+			#win.addch(last[0], last[1], ' ')
+		def eat_food(self):
+			self.food = []
+			self.score += 1
+			self.moar_food
+			win.addch(food[0], food[1], '*')	
+			win.addch(snake[0][0], snake[0][1], '#')
+
+
+		def moar_food(self):
+			while self.food == []:
+				food = [randint(1, size_y - 2), randint(1, size_x - 2)] # à changer, il faudrait faire un random int sur là où est le snek pour ne pas avoir à refaire les calculs à l'infini
 				if food in snake: food = []
-			win.addch(food[0], food[1], '*')
-		else:
-			last = snake.pop()										  # [1] If it does not eat the food, length decreases
-			win.addch(last[0], last[1], ' ')
-		win.addch(snake[0][0], snake[0][1], '#')
 
 	curses.endwin()
 	print("\nScore - " + str(score))
 
+
+	def play(self):
+		    while 1:
+		        move = self.snek.tick()
+		        if move == 'food':
+		            self.eat_food()
+		        if move == 'lose': break
+	time.sleep(0.3)
+
 class snekbot:
+
+	pos = game.snake
+
+	def __init__(self,game):
+		self.game=game
+		self.pos = [(1,1),(1,2)]#.append((0,0))
+		self.weight_food = random.uniform(0,5)
+		#self.weight_selfd = random.uniform(-5,0)
+
+	def play(self):
+		sneks = sneks([])		
+		for snek in sneks:
+
+
+	def tick(self):
+		m = self.sickestest_move()
+		if m == None: return 'lose'
+		elif m == self.game.food:
+			self.bigger(m)S
+			return 'food'
+		else:
+			self.move(m)
+			return ''
+			
+			
+	def move(self,to):
+		self.pos.pop()
+		self.pos.insert(0,to)
+
+	def bigger(self,to):
+		self.pos.insert(0,to)
+
+	def weight_move(self,move):
+        w = self.weight_food* self.game.score(move)
+        return w #+ (self.weight_selfd*self.distance_score(move))
 
 	# Returns the score
 	def fitness (score):
@@ -79,7 +128,11 @@ class snekbot:
 	Sneks
 	"""
 	# Population
-	def sneks(self):
+	def sneks(sneks,self):
+		if sneks == []:
+			// créer population
+		else:
+			// prendre les premiers sneks, et générer aléatoirement les autres
 		return
 	# Random
 	def random_snek(self):
@@ -88,18 +141,19 @@ class snekbot:
 	def weird_snek(self):
 		return
 	# Selection
-	def most_stronk_snek(self):
+	def most_stronk_snek(sneks_score, self):
 		return
 	# All availables movements
 	def sick_moves(self):
 		moves = []
-		moves.append([snake[0][0] + 1, snake[0][1]])
-		moves.append([snake[0][0] - 1, snake[0][1]])
-		moves.append([snake[0][0], snake[0][1] + 1])
-		moves.append([snake[0][0], snake[0][1] - 1])
+		moves.append([pos[0][0] + 1, pos[0][1]])
+		moves.append([pos[0][0] - 1, pos[0][1]])
+		moves.append([pos[0][0], pos[0][1] + 1])
+		moves.append([pos[0][0], pos[0][1] - 1])
 
 		for move in moves:
-			if move == snake[1]:
+			if move == snake[1] and (move[0] <= 0) and (move[0] > self.game.grid_size[0]) and 
+(move[1] <= 0) and (move[1] > self.game.grid_size[1])):
 				moves.remove(move)
 				break
 		return moves
@@ -108,9 +162,12 @@ class snekbot:
 	def sickestest_move(self):
 		tab2 =[[]]
 		tab = self.sick_moves()
+		#if taille = 0, on perd
+		if len(tab) == 0: return None
+ 
 		for move in tab:
 			tab2.append(poid(move), move)
 		tab2.sort
 		return tab2[0][1]
 
-
+#https://github.com/stephennancekivell/growing-snakes/blob/master/snake.py
