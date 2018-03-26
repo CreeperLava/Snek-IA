c531861052bd0e4ae17532a348509f8c6fb149b8
antoinezak <antoinezak195@gmail.com>
2018-03-26 15:32:15 +0200

    26/03

--- heuristic.rb
+++ heuristic.rb
@@ -10,35 +10,13 @@ include './snek.rb'
 
 class Heuristic
 
-	def initialize(snek,weights,game)	
-		@snek=snek
-		@game_sim=game
-		@head = snek.head	
-		@heuristic = [][]
+	def initialize
+		@nb_iterations = 1000
+		@heuristic = []
 
-		@food= game.food_pos                          #<---------------
-		#Tableau des différents moves possible du snek
-		#@moves = [[(head[0]+1),head[1]],
-		#[head[0],(head[1]+1)],
-		#[head[0],(head[1]-1)],
-		#[(head[0]-1),head[1]]
-		#]
-		@best_fit=["",0]
-
-		@moves=["UP","DOWN","LEFT","RIGHT"]
-		@moves.each  { |m| 
-			@game_sim.next_frame(m)	
-			@fitness=calcFitness	
-			if @fitness > @best_fit[1]
-				@best_fit[0]=m
-				@best_fit[1]=@fitness
-			end
-			@game_sim=game	
-			}
-		
-
-		
-
+		@moves=["e\[A","e\[B","e\[C","e\[D"]   # up, down, right, left
+		@food= game.food_pos
+		random = Random.new
 	end
 	
 
@@ -46,8 +24,8 @@ class Heuristic
 	def calcFitness	
 
 		
-		heuristic[0]=weights[1]*@game_sim.distance_from_food
-		heuristic[1]=weights[2]*score #les murs plus lui-même
+		heuristic[0]= @snek.weights[0]*@game_sim.distance_from_food
+		heuristic[1]=@snek.weights[1]*@game_sim.score 
 			
 		for i in @heuristic.length
 			@fitness+=heuristic[i]
@@ -57,15 +35,74 @@ class Heuristic
 		return @fitness
 	end
 
-
-	def distance_from_food
-		
-		@xDist = abs(food.x - this.snek.x) / game.scale;
-		@yDist = abs(food.y - this.snek.y) / game.scale;
-		return (xDist + yDist);
-	end
-
-	def score
+	def rand_population(n)
+		pop=[]
+		n.times{
+			weight=[]
+			heuristic.length.times weight.push random.rand(5)	
+			pop.push Snek.new(((@game.@size_x)/2),((@game.@size_y)/2), weight)
 			
-	
+		}
+		return pop
 	end
+
+	def genetic_algorithm
+	
+		population= rand_population(50)
+		@nb_iterations.times {
+		#meilleur individu
+		sickestest_snek = best(population)
+		#nouvelle population
+		}
+
+		
+	end
+
+	def one_move
+		game_sim=@game_snek
+
+		best_fit=["",0]
+
+		#faire jouer le snek
+		@moves.each do |m| 
+			@game_sim.next_frame(m)	
+			@fitness=calcFitness	
+			if @fitness > @best_fit[1]
+				best_fit[0]=m
+				best_fit[1]=@fitness
+			end
+			@game_sim=game_snek
+		end	
+		return best_fit[0]
+	end	
+
+	def best(pop)
+		@game = Game.new(true, true, snek)
+		@score_pop = Hash.new
+		
+		pop.each do |snek| 
+			@game_snek = @game
+			
+			# On joue jusqu'à la mort 
+			@game_snek.next_frame one_move until @game_snek.game_over?				
+			
+
+			# le snek est mort, l'ajouter à la hash map
+			@score_pop[snek] = game_snek.score
+		end
+		sickestest_snek = sickestest(@score_pop)
+	end 
+	
+	def sickestest(pop)
+		max=0
+		pop.each do |key, value| 
+			if value > max	do 				
+				max = value 
+				snek = key
+			end 	
+		end	
+		return snek
+	end
+end
+
+
