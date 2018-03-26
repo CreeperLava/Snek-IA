b17ae1d94c2bc8d1df7ea9c3e26bde248daf2785
antoinezak <antoinezak195@gmail.com>
2018-03-26 13:58:41 +0200

    heuristic lié au jeu

--- heuristic.rb
+++ heuristic.rb
@@ -12,8 +12,7 @@ class Heuristic
 
 	def initialize(snek,weights,game)	
 		@snek=snek
-		@game=game
-		@fitness=0
+		@game_sim=game
 		@head = snek.head	
 		@heuristic = [][]
 
@@ -24,6 +23,21 @@ class Heuristic
 		#[head[0],(head[1]-1)],
 		#[(head[0]-1),head[1]]
 		#]
+		@best_fit=["",0]
+
+		@moves=["UP","DOWN","LEFT","RIGHT"]
+		@moves.each  { |m| 
+			@game_sim.next_frame(m)	
+			@fitness=calcFitness	
+			if @fitness > @best_fit[1]
+				@best_fit[0]=m
+				@best_fit[1]=@fitness
+			end
+			@game_sim=game	
+			}
+		
+
+		
 
 	end
 	
@@ -32,8 +46,8 @@ class Heuristic
 	def calcFitness	
 
 		
-		heuristic[0]=weights[1]*distance_from_food()
-		heuristic[1][=weights[2]*dead_end() #les murs plus lui-même
+		heuristic[0]=weights[1]*@game_sim.distance_from_food
+		heuristic[1]=weights[2]*score #les murs plus lui-même
 			
 		for i in @heuristic.length
 			@fitness+=heuristic[i]
@@ -44,9 +58,14 @@ class Heuristic
 	end
 
 
-	def distance_from_food()
+	def distance_from_food
 		
 		@xDist = abs(food.x - this.snek.x) / game.scale;
 		@yDist = abs(food.y - this.snek.y) / game.scale;
 		return (xDist + yDist);
+	end
+
+	def score
+			
+	
 	end
