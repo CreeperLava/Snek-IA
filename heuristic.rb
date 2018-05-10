a387df851081c620b9e5b942c1576128c9fdd29f
antoinezak <37288453+antoinezak@users.noreply.github.com>
2018-05-10 21:15:06 +0200

    Update heuristic.rb

--- heuristic.rb
+++ heuristic.rb
@@ -37,8 +37,8 @@ class Heuristic
 
 	#Fitnesse pour chacun des moves du snek
 	def calcFitness(game_sim)
-		@heuristic[0] = @snek.weights[0]*game_sim.distance_from_food
-		@heuristic[1] = @snek.weights[1]*game_sim.score 
+		@heuristic[0] = game_sim.snek.weights[0]*game_sim.distance_from_food
+		@heuristic[1] = game_sim.snek.weights[1]*game_sim.score 
 			
 		@heuristic.each do |h|
 			@fitness.push h
